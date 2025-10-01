05a05dabff7f614fe821ce5e14bf952fd32c7ba8
Cristian <cleobla0802@iescarrillo.es>
2025-10-01 12:35:11 +0200

    Inserccion de una nueva funcion "mostrarMesasDisponibles"

--- src/feature/table-list.ts
+++ src/feature/table-list.ts
@@ -2,14 +2,22 @@
 // Feature: Listado de Mesas
 // -----------------------------
 interface Mesa {
-numero: number;
-capacidad: number;
-ocupada: boolean;
+  numero: number;
+  capacidad: number;
+  ocupada: boolean;
 }
 
 let mesas: Mesa[] = [
-{ numero: 1, capacidad: 2, ocupada: false },
-{ numero: 2, capacidad: 4, ocupada: true },
-{ numero: 3, capacidad: 6, ocupada: false }
+  { numero: 1, capacidad: 2, ocupada: false },
+  { numero: 2, capacidad: 4, ocupada: true },
+  { numero: 3, capacidad: 6, ocupada: false },
 ];
 
+export function mostrarMesasDisponibles(): void {
+  console.log(" Mesas disponibles:");
+  mesas.forEach((mesa) => {
+    if (!mesa.ocupada) {
+      console.log(`Mesa ${mesa.numero} (Capacidad: ${mesa.capacidad})`);
+    }
+  });
+}
