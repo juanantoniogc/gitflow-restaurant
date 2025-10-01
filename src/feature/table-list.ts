314c4bcd56ddaff7307ccd41b4d32adde2285cf7
Cristian <cleobla0802@iescarrillo.es>
2025-10-01 12:30:45 +0200

    Inserccion de datos en atributos de la clase objeto mesa

---
+++ src/feature/table-list.ts
@@ -0,0 +1,15 @@
+// -----------------------------
+// Feature: Listado de Mesas
+// -----------------------------
+interface Mesa {
+numero: number;
+capacidad: number;
+ocupada: boolean;
+}
+
+let mesas: Mesa[] = [
+{ numero: 1, capacidad: 2, ocupada: false },
+{ numero: 2, capacidad: 4, ocupada: true },
+{ numero: 3, capacidad: 6, ocupada: false }
+];
+
