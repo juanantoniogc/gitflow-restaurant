3f6ca0d5fc7f41d07e8d282db874f3277f24c46b
Juan Antonio <jgarcab612@iescarrillo.es>
2025-10-08 11:19:37 +0200

    Modificación de los archivos package.json, main.ts personalizandolo y la creacion del changelog.md

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { login } from "./feature/login";
 import { mostrarMesasDisponibles } from "./feature/table-list";
 console.log("===== SISTEMA DE RESERVAS v1.0.0 =====");
+console.log("Buenos dias");
 const acceso = login("ana@example.com", "1234");
 if (acceso) {
 console.log("Acceso concedido ");
